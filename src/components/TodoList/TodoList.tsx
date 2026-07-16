0b9f137365642ccd672a67c7f287d2adaaeb7a8a
Rodrigo <sistsayao@gmail.com>
2026-07-15 21:38:51 -0300

    Refactor imports and simplify TodoList component

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,7 +1,5 @@
-
-
-import { Todo } from '../../types/Todo';
-import { User } from '../../types/User';
+import { Todo } from '../../types/Todo.ts';
+import { User } from '../../types/User.ts';
 import { TodoInfo } from '../TodoInfo/TodoInfo';
 
 type Props = {
@@ -9,17 +7,11 @@ type Props = {
   users: User[];
 };
 
-
-export const TodoList: React.FC<Props> = ({
-  todos,
-  users,
-}) => {
+export const TodoList: React.FC<Props> = ({ todos, users }) => {
   return (
     <section className="TodoList">
       {todos.map(todo => {
-        const user = todo.user || users.find(
-          item => item.id === todo.userId,
-        );
+        const user = todo.user || users.find(item => item.id === todo.userId);
 
         if (!user) {
           return null;
