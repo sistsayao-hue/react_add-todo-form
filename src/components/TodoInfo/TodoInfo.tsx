7537fb312d5b40c417f37ae45b401afe338f5911
Rodrigo <sistsayao@gmail.com>
2026-07-15 21:36:22 -0300

    Update TodoInfo.tsx

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -7,6 +7,42 @@ type Props = {
   todo: Todo;
 };
 
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  return (
+    <article
+      data-id={todo.id}
+      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <UserInfo user={todo.user} />
+    </article>
+  );
+};
+import { Todo } from '../../types/Todo.ts';
+import { UserInfo } from '../UserInfo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoInfo: React.FC<Props> = ({ todo }) => {
+  return (
+    <article
+      data-id={todo.id}
+      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <UserInfo user={todo.user} />
+    </article>
+  );
+};
+
+type Props = {
+  todo: Todo;
+};
+
 export const TodoInfo: React.FC<Props> = ({
   todo,
 }) => {
